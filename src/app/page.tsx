2b4947e9723639f994c29b7dc224a05dd822e41b
TM AXO <tuminh1@axo.business>
2023-08-21 17:29:33 +0700

    feat: custom landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,48 @@
 'use client';
 
-import Head from 'next/head';
-import * as React from 'react';
+import { OrbitControls, Sphere } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
 
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
+import pointsInner from '@/app/utils';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Logo from '~/svg/Logo.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
-export default function HomePage() {
+const HomePage = () => {
   return (
-    <main>
-      <Head>
-        <title>Hi</title>
-      </Head>
-      <section className='bg-white'>
-        <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
-
-          <ButtonLink className='mt-6' href='/components' variant='light'>
-            See all components
-          </ButtonLink>
-
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
-            />
-          </UnstyledLink>
-
-          <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
-          </footer>
-        </div>
-      </section>
-    </main>
+    <div className='relative'>
+      <Canvas className='h-screen bg-[#101010]' style={{ height: '100hv' }}>
+        <OrbitControls />
+        <directionalLight />
+        <pointLight position={[-30, 0, -30]} power={10.0} />
+        <PointCircle />
+      </Canvas>
+    </div>
   );
-}
+};
+
+const PointCircle = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => {
+    ref.current!.rotation.z = clock.getElapsedTime() * 0.05;
+  });
+  return (
+    <group ref={ref}>
+      {pointsInner.map((point) => (
+        <Point key={point.idx} position={point.position} color={point.color} />
+      ))}
+    </group>
+  );
+};
+
+const Point = ({ position, color }) => {
+  return (
+    <Sphere position={position} args={[0.1, 10, 10]}>
+      <meshStandardMaterial
+        color={color}
+        emissive={color}
+        emissiveIntensity={0.5}
+        roughness={0.5}
+      />
+    </Sphere>
+  );
+};
+export default HomePage;
