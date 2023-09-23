c4b3c7fd5f4e59f1b848837bc44dce774d3cb1dc
thanh <123854795+ThanhCao98@users.noreply.github.com>
2023-09-23 16:45:12 +0700

    update code

--- src/app/components/SectionConnect.tsx
+++ src/app/components/SectionConnect.tsx
@@ -13,7 +13,7 @@ export default function SectionConnect() {
         </div>
       </div>
 
-      <div className='fade-skew mt-[100px]'>
+      <div className='fade-skew'>
         <div className='relative'>
           <SliderScroll />
         </div>
